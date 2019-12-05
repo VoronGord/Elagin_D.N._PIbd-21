eda20420e181c7ec61ac55da089cdbe67ff3b093
VoronGord <56074431+VoronGord@users.noreply.github.com>
2019-12-05 22:55:46 +0400

    создание корабля, кнопок для передвижение,его отрисовка

--- WindowsFormsShips/FormLincor.cs
+++ WindowsFormsShips/FormLincor.cs
@@ -12,9 +12,49 @@ namespace WindowsFormsShips
 {
     public partial class FormLincor : Form
     {
+        private Lincor lincor;
+
+
         public FormLincor()
         {
             InitializeComponent();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxShip.Width, pictureBoxShip.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            lincor.DrawLincor(gr);
+            pictureBoxShip.Image = bmp;
+        }
+
+        private void GenerLincor_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            lincor = new Lincor(rnd.Next(10, 20), rnd.Next(5, 8), Color.Blue, Color.Yellow, true, true, true);
+            lincor.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxShip.Width, pictureBoxShip.Height); Draw();
+        }
+
+
+        private void buttonMove_Click(object sender, EventArgs e)
+        {//получаем имя кнопкиs
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "Up":
+                    lincor.MoveTransport(Direction.Up);
+                    break;
+                case "Down":
+                    lincor.MoveTransport(Direction.Down);
+                    break;
+                case "Left":
+                    lincor.MoveTransport(Direction.Left);
+                    break;
+                case "Right":
+                    lincor.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
         }
     }
 }
