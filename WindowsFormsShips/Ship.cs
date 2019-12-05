dcc6e30ea2ff24593ba8aef2aa1eaeb83ddf21a7
VoronGord <56074431+VoronGord@users.noreply.github.com>
2019-12-05 23:13:29 +0400

    лаба2

---
+++ WindowsFormsShips/Ship.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsShips
+{
+    public abstract class Ship : IShip
+    {         /// <summary>         /// Левая координата отрисовки автомобиля         /// </summary>  
+        protected float _startPosX;
+
+        /// <summary>         /// Правая кооридната отрисовки автомобиля         /// </summary>    
+        protected float _startPosY;
+
+        /// <summary>         /// Ширина окна отрисовки         /// </summary>    
+        protected int _pictureWidte;
+
+        //Высота окна отрисовки         
+        protected int _pictureHeight;
+
+        /// <summary>         /// Максимальная скорость         /// </summary>         
+        public int MaxSpeed { protected set; get; }
+
+        /// <summary>         /// Вес автомобиля         /// </summary>         
+        public float SpeedWater { protected set; get; }
+
+        /// <summary>         /// Основной цвет кузова         /// </summary>         
+        public Color MainColor { protected set; get; }
+
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidte = width;
+            _pictureHeight = height;
+        }
+
+        public abstract void DrawShip(Graphics g);
+
+        public abstract void MoveTransport(Direction direction);
+    }
+}
